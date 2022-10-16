96462a678b9571adba5055c0ee4cc941eb18fe53
Edgar Carrillo Estrada <edgar.carrillo.e@gmail.com>
2022-10-16 10:01:58 -0500

    Syntax analysis finished
    All the Syntax analysis has been finished correctly until this part.
    
    If in the next part of the symbol table you have any problem, you can create a branch from this point to restart from a fresh checkpoint.
    
    Also here's a script that helps you to compile de JJ file and analize all the TXT that the teacher gave us. Modify if necessary because the output rewrites itself in the "temporal.txt" file

--- Exe1.bat
+++ Exe1.bat
@@ -2,6 +2,29 @@ call javacc cminusparser.jj
 timeout /T 1 /nobreak
 call javac cminusparser.java
 timeout /T 1 /nobreak 
-java cminusparser < "ERROR codigo3 - ES6.txt" > "Prueba de analisis ERROR codigo3-6.txt"
-taskkill /IM notepad.exe
-"Prueba de analisis ERROR codigo3-6.txt"
+echo Codigo correcto 1
+java cminusparser < "CORRECTO codigo1.txt" > "temporal.txt"
+echo Codigo correcto 2
+java cminusparser < "CORRECTO codigo2.txt" > "temporal.txt"
+echo Codigo correcto 3
+java cminusparser < "CORRECTO codigo3.txt" > "temporal.txt"
+echo Codigo correcto 4
+java cminusparser < "CORRECTO codigo4.txt" > "temporal.txt"
+echo Codigo con error 1-1
+java cminusparser < "ERROR codigo1 - ES1.txt" > "temporal.txt"
+echo Codigo con error 2-2
+java cminusparser < "ERROR codigo2 - ES2.txt" > "temporal.txt"
+echo Codigo con error 3-3
+java cminusparser < "ERROR codigo3 - ES3.txt" > "temporal.txt"
+echo Codigo con error 1-4
+java cminusparser < "ERROR codigo1 - ES4.txt" > "temporal.txt"
+echo Codigo con error 2-5
+java cminusparser < "ERROR codigo2 - ES5.txt" > "temporal.txt"
+echo Codigo con error 3-6
+java cminusparser < "ERROR codigo3 - ES6.txt" > "temporal.txt"
+echo Codigo con error 1-7
+java cminusparser < "ERROR codigo1 - ES7.txt" > "temporal.txt"
+echo Codigo con error 2-8
+java cminusparser < "ERROR codigo2 - ES8.txt" > "temporal.txt"
+pause
+"temporal.txt"
